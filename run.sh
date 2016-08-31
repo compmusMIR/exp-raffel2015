bece94a9bf516889b8461b2382eccd7965d2eb70
Fabio Gorodscy <fabiogo@outlook.com>
2016-08-31 15:41:55 +0000

    Add run.sh

---
+++ run.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+PYTHON="/usr/bin/python2.7"
+
+cd ./midi_dataset
+
+cd scripts
+
+$PYTHON create_whoosh_indices.py
+
+mkdir ../results
+touch ../results/text_matches.js
+
+$PYTHON text_match_datasets.py
